f3c2d2dfb6091aad8dc9d55ce99aa3530b7ad993
Austin Hauter <hautera@me.com>
2026-08-30 11:50:01 -0700

    ast parsing! omg

--- cmd/parse/main.go
+++ cmd/parse/main.go
@@ -1,9 +1,40 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"os"
+
+	sitter "github.com/smacker/go-tree-sitter"
+	"github.com/smacker/go-tree-sitter/golang"
 )
+
+const file_path = "./data/thirdRun/gin/c6d07dcc1ee884cf3298855ce05803637010a085/mode.go"
+
+func getSource() []byte {
+	result, err := os.ReadFile(file_path)
+	if err != nil {
+		panic("Could not read the file!")
+	}
+	return result
+}
 
 func main() {
 	fmt.Println("hello world")
+	parser := sitter.NewParser()
+	parser.SetLanguage(golang.GetLanguage())
+	source := getSource()
+	tree, err := parser.ParseCtx(context.Background(), nil, source)
+	if err != nil {
+		panic("Error parsing tree")
+	}
+
+	n := tree.RootNode()
+
+	fmt.Println(n)
+	fmt.Println(n.ChildCount())
+	child := n.NamedChild(10)
+	fmt.Println(child.Type())
+	fmt.Println(child.StartByte())
+	fmt.Println(child.EndByte())
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/ahauter/astrolabev2
 
 go 1.25.0
 
-require golang.org/x/sync v0.22.0 // indirect
+require (
+	github.com/smacker/go-tree-sitter v0.0.0-20240827094217-dd81d9e9be82 // indirect
+	golang.org/x/sync v0.22.0 // indirect
+)
